444b3f0017f4dde8c079063219763b769bf7dff8
pavanprostack <pavanprostack@gmail.com>
2022-11-22 15:10:43 +0530

    kk

--- InBuiltModules/OperatingSystem/os.js
+++ InBuiltModules/OperatingSystem/os.js
@@ -1,13 +1,15 @@
 const os = require('os')
 
-console.log(os.version());
-console.log(os.arch());
-console.log(os.platform())
-console.log(os.freemem())
-console.log(os.hostname())
-console.log(os.release())
-console.log(os.type())
-console.log(os.uptime())
-console.log(os.userInfo())
-console.log(os.homedir())
+// console.log(os.version());
+// console.log(os.arch());
+// console.log(os.platform())
+// console.log(os.freemem())
+// console.log(os.hostname())
+// console.log(os.release())
+// console.log(os.type())
+// console.log(os.uptime())
+// console.log(os.userInfo())
+// console.log(os.homedir())
 
+console.log(os.version())
+console.log(os.arch())
