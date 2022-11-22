9843aee8ba2050d2d5f24d452a4ef23877cb4bd6
pavanprostack <pavanprostack@gmail.com>
2022-11-22 11:57:47 +0530

    okk

--- InBuiltModules/httpServer/webSite/server.js
+++ InBuiltModules/httpServer/webSite/server.js
@@ -8,25 +8,25 @@ const url = require('url')
 let file = path.join(__dirname, "config", "config.env")
 console.log(file)
 
-dotenv.config({path:file})
+dotenv.config({ path: file })
 
 
-let PORT=process.env.PORT
-let host_Name=process.env.HOST_NAME
+let PORT = process.env.PORT;
+let HOST_NAME = process.env.HOST_NAME;
 // console.log(port)
 // console.log(hostName)
 
 
-let server = http.createServer((request, response)=>{
+let server = http.createServer((request, response) => {
     let url = request.url;
     console.log(url)
-    response.writeHead(200, {'content-type':'text/html'})
-    fs.readFile('views/index.html', 'utf-8', (err, data)=>{
-        if(err) throw err
-        response.end(data)
+    response.writeHead(200, { 'content-type': 'text/html' });
+    fs.readFile('./views/index.html', 'utf-8', (err, data) => {
+        if (err) throw err;
+        response.end(data);
     })
 });
 
-server.listen( ()=>{
-    console.log(`Server is running on.... http://${host_Name}:${PORT}`)
+server.listen(PORT, () => {
+    console.log(`Server is running on.....http://${HOST_NAME}:${PORT}`)
 })
