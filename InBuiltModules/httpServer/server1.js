5ef529b0d32dde619e271f0254336968d2368ce1
pavanprostack <pavanprostack@gmail.com>
2022-11-22 19:25:02 +0530

    okkk

--- InBuiltModules/httpServer/server1.js
+++ InBuiltModules/httpServer/server1.js
@@ -7,16 +7,17 @@ let file = path.join(__dirname, "config", "config.env")
 dotenv.config({ path: file })
 
 let port = process.env.PORT
-let hostName = process.env.HOST_NAME
+// let hostName = process.env.HOST_NAME
 
 console.log(port)
-console.log(hostName)
+// console.log(hostName)
+
 
 let server = http.createServer((request, response) => {
     response.writeHead(200, { 'content-type': 'text/html'});
-    response.end("<h1>Hello!, Enjoy the day</h1>")
+    response.end(`<h1>Hello!, Enjoy the day</h1>`)
 });
 
-server.listen(() => {
-    console.log(`Server is running on...http//${hostName}:${port}`)
+server.listen(port, () => {
+    console.log(`Server is running on...${port}`)
 })
--- InBuiltModules/httpServer/webSite/server.js
+++ InBuiltModules/httpServer/webSite/server.js
@@ -6,27 +6,52 @@ const url = require('url')
 
 
 let file = path.join(__dirname, "config", "config.env")
-console.log(file)
+// console.log(file)
 
 dotenv.config({ path: file })
 
 
 let PORT = process.env.PORT;
 let HOST_NAME = process.env.HOST_NAME;
-// console.log(port)
-// console.log(hostName)
+console.log(PORT)
+console.log(HOST_NAME)
 
 
 let server = http.createServer((request, response) => {
     let url = request.url;
     console.log(url)
     response.writeHead(200, { 'content-type': 'text/html' });
-    fs.readFile('./views/index.html', 'utf-8', (err, data) => {
-        if (err) throw err;
-        response.end(data);
-    })
+
+    if(url=="/"){
+        fs.readFile('./views/index.html', 'utf-8', (err, data) => {
+            if (err) throw err;
+            response.end(data);
+        })
+    }
+
+    if(url=="/about"){
+        fs.readFile("./views/about.html", "utf-8", (err, data)=>{
+            if(err) throw err
+            response.end(data)
+        })
+    }
+
+    if(url=="/services"){
+        fs.readFile("./views/services.html", "utf-8", (err, data)=>{
+            if(err) throw err
+            response.end(data)
+        })
+    }
+
+    if(url=="/contact"){
+        fs.readFile("./views/contact.html", "utf-8", (err, data)=>{
+            if(err) throw err
+            response.end(data)
+        })
+    }
+    
 });
 
 server.listen(PORT, () => {
-    console.log(`Server is running on.....http://${HOST_NAME}:${PORT}`)
+    console.log(`Server is running on.....:${PORT}`)
 })
