25c5e0fd7693fc210db9040b2315cd324bb8ee9b
pavanprostack <pavanprostack@gmail.com>
2022-11-30 16:59:32 +0530

    mongodb

---
+++ Product-CRUD/app.js
@@ -0,0 +1,49 @@
+import express, { urlencoded } from 'express'
+import morgan from 'morgan'
+import chalk from 'chalk'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import productRouter from './router/productRouter.js'
+
+const app = express();
+
+dotenv.config({ path: "./config/config.env" });
+
+let port = process.env.PORT;
+let hostname = process.env.HOST_NAME;
+
+
+console.log(port);
+
+app.use(morgan('tiny'));
+
+app.use(cors());
+
+//How To Read Form Data
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
+//create route request
+app.get("/", (request, response)=>{
+    response.send("<h1>This is Nodejs Express App Connecting With MongoDB</h1>")
+});
+
+
+//configure product routes
+app.use("/product", productRouter)
+
+const mongo_url = process.env.MONGO_DB_LOCAL_URL
+mongoose.connect(mongo_url)
+    .then((response) => {
+        console.log(`Mongo DB - Connection Successfull!`)
+    })
+    .catch((err) => {
+        console.log(err)
+        process.exit(1)
+    })
+
+app.listen(()=>{
+    console.log(`Server is running on.....http://${hostname}:${port}`)
+})
