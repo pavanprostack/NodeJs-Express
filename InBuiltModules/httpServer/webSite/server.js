9cca89c7de4199f10af137ccac2a5205877df6b3
pavanprostack <pavanprostack@gmail.com>
2023-01-11 17:36:06 +0530

    http url

--- InBuiltModules/httpServer/webSite/server.js
+++ InBuiltModules/httpServer/webSite/server.js
@@ -4,81 +4,87 @@ const path = require('path')
 const dotenv = require('dotenv')
 const url = require('url')
 
-
-let file = path.join(__dirname, "config", "config.env")
-// console.log(file)
-
 dotenv.config({ path: "./config/config.env" })
 
 
-let PORT = process.env.PORT;
-let HOST_NAME = process.env.HOST_NAME;
-console.log(PORT)
-console.log(HOST_NAME)
+// let PORT = process.env.PORT;
+// let HOST_NAME = process.env.HOST_NAME;
+// console.log(PORT)
+// console.log(HOST_NAME)
 
 
-let server = http.createServer((request, response) => {
-    let url = request.url;
-    console.log(url)
-    response.writeHead(200, { 'content-type': 'text/html' });
+// let server = http.createServer((request, response) => {
+//     let url = request.url;
+//     console.log(url)
+//     response.writeHead(200, { 'content-type': 'text/html' });
 
-    if(url=="/"){
-        fs.readFile('./views/index.html', 'utf-8', (err, data) => {
-            if (err) throw err;
-            response.end(data);
-        })
-    }
+//     if(url=="/"){
+//         fs.readFile('./views/index.html', 'utf-8', (err, data) => {
+//             if (err) throw err;
+//             response.end(data);
+//         })
+//     }
 
-    if(url=="/about"){
-        fs.readFile("./views/about.html", "utf-8", (err, data)=>{
-            if(err) throw err
-            response.end(data)
-        })
-    }
+//     if(url=="/about"){
+//         fs.readFile("./views/about.html", "utf-8", (err, data)=>{
+//             if(err) throw err
+//             response.end(data)
+//         })
+//     }
 
-    if(url=="/services"){
-        fs.readFile("./views/services.html", "utf-8", (err, data)=>{
-            if(err) throw err
-            response.end(data)
-        })
-    }
+//     if(url=="/services"){
+//         fs.readFile("./views/services.html", "utf-8", (err, data)=>{
+//             if(err) throw err
+//             response.end(data)
+//         })
+//     }
 
-    if(url=="/contact"){
-        fs.readFile("./views/contact.html", "utf-8", (err, data)=>{
-            if(err) throw err
-            response.end(data)
-        })
-    }
+//     if(url=="/contact"){
+//         fs.readFile("./views/contact.html", "utf-8", (err, data)=>{
+//             if(err) throw err
+//             response.end(data)
+//         })
+//     }
     
-});
+// });
 
-server.listen(PORT, () => {
-    console.log(`Server is running on.....http://localhost:${PORT}`)
-})
+// server.listen(PORT, () => {
+//     console.log(`Server is running on.....http://localhost:${PORT}`)
+// })
 
 
 
 // Practice
 
-// const server1=http.createServer((req, res)=>{
-//      url = req.url;
-//     res.writeHead(200, {"contentType" : "text/html"});
+let port = process.env.PORT
 
-//     if(url="/"){
-//         fs.readFile("./views/index.html", "utf-8", (err, data)=>{
-//             if(err) throw err;
-//             console.log(data);
-//         })
-//     }
+const server=http.createServer((req, res)=>{
+    const url = req.url;
+    res.writeHead(200, {"content-type":"text/html"});
 
-//     if(url="/about"){
-//         fs.readFile("./views/about.html", "utf-8", (err, data)=>{
-//             if(err) throw err;
-//             console.log(data);
-//         })
-//     }
-// })
+    if(url=="/"){
+        fs.readFile("views/index.html", "utf-8", (err, data)=>{
+            if(err) throw err;
+            res.end(data)
+        })
+    }
+    if(url=="/about"){
+        fs.readFile("views/about.html", "utf-8", (err, data)=>{
+            res.end(data)
+        })
+    }
+    if(url=="/contact"){
+        fs.readFile("views/contact.html", "utf-8", (err, data)=>{
+            res.end(data)
+        })
+    }
+    if(url=="/services"){
+        fs.readFile("views/services.html", "utf-8", (err, data)=>{
+            res.end(data)
+        })
+    }
+})
 
-// server1.listen(PORT, ()=>{
-//     console.log(`Server is running on http://localhost:${PORT}`);
-// })
+server.listen(port, ()=>{
+    console.log(`Server is running on http://localhost:${port}`)
+})
