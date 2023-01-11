1611812ed01fde904af74898ff6d816819247815
pavanprostack <pavanprostack@gmail.com>
2023-01-11 21:17:21 +0530

    bcrypt

--- ThirdParty-Modules/bcrypt/user.js
+++ ThirdParty-Modules/bcrypt/user.js
@@ -1,50 +1,53 @@
 const bcrypt = require('bcrypt')
 
-let user = {
-    id:101,
-    userName:"pavan",
-    password:"pavankalyan",
-    cc:"1234123412341234"
-}
+// let user = {
+//     id:101,
+//     userName:"pavan",
+//     password:"pavankalyan",
+//     cc:"1234123412341234"
+// }
 
-let salt = bcrypt.genSaltSync(10);
-let new_password = bcrypt.hashSync(user.password, salt)
+// let salt = bcrypt.genSaltSync(10);
+// let new_password = bcrypt.hashSync(user.password, salt)
 
-console.log(user.password);
-console.log(new_password);
+// console.log(user.password);
+// console.log(new_password);
 
-let flag = bcrypt.compareSync("pavankalyan", new_password)
-console.log(flag)
+// let flag = bcrypt.compareSync("pavankalyan", new_password)
+// console.log(flag)
 
-if(flag){
-    console.log("Login Successfully")
-}
-else{
-    console.log("Login Failed")
-}
+// if(flag){
+//     console.log("Login Successfully")
+// }
+// else{
+//     console.log("Login Failed")
+// }
 
 
 
 
 // Practice
 
-let student={
-    id:104,
-    userName:"pk",
-    password:"subbu",
-    cc:"1234123412341235"
+let students = {
+    id:101,
+    userName:"pavan",
+    password:"pavankalyan",
+    cc:"1234123412341234"
 }
 
+
 let salt1=bcrypt.genSaltSync(10);
-let new_password1=bcrypt.hashSync(student.password, salt1)
+let newPassword=bcrypt.hashSync(students.password, salt1);
 
-console.log(student.password);
-console.log(new_password1);
+console.log(students.password)
+console.log(newPassword)
 
-let flag1 = bcrypt.compareSync("54546", new_password1);
+let flag1=bcrypt.compareSync("pavankalyan", newPassword);
+console.log(flag1)
 
 if(flag1){
-    console.log("login succefully.")
-}else{
-    console.log("login failed.")
+    console.log("Login Succesful")
+}
+else{
+    console.log("Login Failed")
 }
